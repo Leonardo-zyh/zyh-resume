4e6e5ceeb71ea3ac2e5672de20cea9f2ca8e8790
Leonardo_zyh <956731563@qq.com>
2018-08-21 10:51:17 +0800

    Message

--- js/message.js
+++ js/message.js
@@ -6,9 +6,43 @@ AV.init({
   appKey: APP_KEY
 });
 
+var query = new AV.Query('Message');
+query.find().then(function (message) {
+// console.log(message[0].attributes)
+ let array = message.map((item)=> item.attributes)
+ array.forEach((item)=>{
+   let li = document.createElement('li')
+   li.innerText =`${item.name}: ${item.content}`
+   let messageList = document.querySelector('#messageList')
+   messageList.appendChild(li)
+ })
+}, function (error) {
+  alert(提交失败)// 异常处理
+});
+
+let myForm = document.querySelector('#postMessageForm')
+
+myForm.addEventListener('submit',function(e){
+  e.preventDefault()
+  let content = myForm.querySelector('input[name=content]').value
+  let name = myForm.querySelector('input[name=name]').value
+  var Message = AV.Object.extend('Message');
+  var message = new Message();
+  message.save({
+    'content': content,
+    'name': name,
+  }).then(function(object){
+    let li = document.createElement('li')
+    li.innerText =`${object.attributes.name}: ${object.attributes.content}`
+    let messageList = document.querySelector('#messageList')
+    messageList.appendChild(li)
+    myForm.querySelector('input[name=content]').value = ''
+    //alert('留言成功')
+  })
+})
 
 
-
+/*
 //创建 TestObject 表
 var TestObject = AV.Object.extend('Message');
 //在表中创建一行数据
@@ -19,4 +53,4 @@ testObject.save({
   words: 'Hello World!'
 }).then(function(object) {
   alert('LeanCloud Rocks!');
-})
+})*/
