efe662702761fd564e1a416af988b29974fa4580
Leonardo_zyh <956731563@qq.com>
2018-08-24 21:22:00 +0800

    OOP

--- js/message.js
+++ js/message.js
@@ -1,4 +1,62 @@
-!function () {
+!function(){
+  var view = View('.message')
+
+  var model = Model({resourceName:'Message'})
+
+  var controller = Controller({
+    messageList: null,
+    form: null,
+    init: function(view,controller){
+      this.messageList = view.querySelector('#messageList')
+      this.form = view.querySelector('form')
+      this.loadMessages()
+    },
+    loadMessages: function () {
+      this.model.fetch().then(
+        (messages) => {
+        let array = messages.map((item) => item.attributes)
+        array.forEach((item) => {
+          let li = document.createElement('li')
+          li.innerText = `${item.name}: ${item.content}`
+          this.messageList.appendChild(li)
+        })
+      }, function (error) {
+        alert(提交失败)// 异常处理
+        
+      })
+
+    },
+    bindEvents: function () {
+      
+      this.form.addEventListener('submit', (e)=> {
+        e.preventDefault() 
+        this.saveMessage()
+      })
+    },
+    saveMessage: function() {
+      let myForm = this.form
+      let content = myForm.querySelector('input[name=content]').value
+      let name = myForm.querySelector('input[name=name]').value
+      this.model.save({
+        'name':name,'content':content
+      }).then(function (object) {
+        let li = document.createElement('li')
+        li.innerText = `${object.attributes.name}: ${object.attributes.content}`
+        let messageList = document.querySelector('#messageList')
+        messageList.appendChild(li)
+        myForm.querySelector('input[name=content]').value = ''
+        //alert('留言成功')
+        console.log(object)
+      })
+    }
+  })
+
+  controller.init(view,model)
+}.call()
+
+
+
+/*!function () {
   
   var model = {
     //获取数据
