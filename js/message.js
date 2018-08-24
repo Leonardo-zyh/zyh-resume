26b257ca08e17bf30f9101c3b629f3a1f2a2f565
Leonardo_zyh <956731563@qq.com>
2018-08-24 11:48:17 +0800

    MVC

--- js/message.js
+++ js/message.js
@@ -1,45 +1,85 @@
-var APP_ID = '30FJxNRGUl44IppYUSj40Thw-gzGzoHsz';
-var APP_KEY = 'SSl3FACzzTQv2pCNMxkNh6T8';
+!function () {
+  
+  var model = {
+    //获取数据
+    init: function () {
+      var APP_ID = '30FJxNRGUl44IppYUSj40Thw-gzGzoHsz';
+      var APP_KEY = 'SSl3FACzzTQv2pCNMxkNh6T8';
+      AV.init({ appId: APP_ID, appKey: APP_KEY });
+    },
+    fetch: function(){
+      var query = new AV.Query('Message');
+      return  query.find()  //Promise对象
+    },
+    //创建数据
+    save: function(name,content){
+      var Message = AV.Object.extend('Message');
+      var message = new Message();
+      return message.save({   //Promise对象
+        'name': name,
+        'content': content,
+      })
+    }
+  }
 
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-});
+  var view = document.querySelector('.message')
 
-var query = new AV.Query('Message');
-query.find().then(function (message) {
-// console.log(message[0].attributes)
- let array = message.map((item)=> item.attributes)
- array.forEach((item)=>{
-   let li = document.createElement('li')
-   li.innerText =`${item.name}: ${item.content}`
-   let messageList = document.querySelector('#messageList')
-   messageList.appendChild(li)
- })
-}, function (error) {
-  alert(提交失败)// 异常处理
-});
+  var controller = {
+    view: null,
+    model: null,
+    messageList: null,
+    init: function (view, model) {
+      this.view = view
+      this.model = model
+      
+      this.messageList = view.querySelector('#messageList')
+      this.form = view.querySelector('form')
+      this.model.init()
+      this.loadMessages()
+      this.bindEvents()
+      
+    },
+   
+    loadMessages: function () {
+      this.model.fetch().then(
+        (messages) => {
+        let array = messages.map((item) => item.attributes)
+        array.forEach((item) => {
+          let li = document.createElement('li')
+          li.innerText = `${item.name}: ${item.content}`
+          this.messageList.appendChild(li)
+        })
+      }, function (error) {
+        alert(提交失败)// 异常处理
+        console.log(1)
+      })
 
-let myForm = document.querySelector('#postMessageForm')
-
-myForm.addEventListener('submit',function(e){
-  e.preventDefault()
-  let content = myForm.querySelector('input[name=content]').value
-  let name = myForm.querySelector('input[name=name]').value
-  var Message = AV.Object.extend('Message');
-  var message = new Message();
-  message.save({
-    'content': content,
-    'name': name,
-  }).then(function(object){
-    let li = document.createElement('li')
-    li.innerText =`${object.attributes.name}: ${object.attributes.content}`
-    let messageList = document.querySelector('#messageList')
-    messageList.appendChild(li)
-    myForm.querySelector('input[name=content]').value = ''
-    //alert('留言成功')
-  })
-})
+    },
+    bindEvents: function () {
+      
+      this.form.addEventListener('submit', (e)=> {
+        e.preventDefault() 
+        this.saveMessage()
+      })
+    },
+    saveMessage: function() {
+      let myForm = this.form
+      let content = myForm.querySelector('input[name=content]').value
+      let name = myForm.querySelector('input[name=name]').value
+      this.model.save(name,content).then(function (object) {
+        let li = document.createElement('li')
+        li.innerText = `${object.attributes.name}: ${object.attributes.content}`
+        let messageList = document.querySelector('#messageList')
+        messageList.appendChild(li)
+        myForm.querySelector('input[name=content]').value = ''
+        //alert('留言成功')
+        console.log(object)
+      })
+    }
+  }
+ 
+    controller.init(view,model)
+  }.call()
 
 
 /*
